d50d4396db584385156f1c76cdafe4396f73dc43
Abdelhamid <3b7ameed@gmail.com>
2019-11-25 00:22:19 +0200

    fixing things

--- program.py
+++ program.py
@@ -5,6 +5,7 @@ import datetime
 db = psycopg2.connect("dbname=news")
 the_cursor = db.cursor()
 
+
 def most_popular_articles():
     the_cursor.execute('''select articles.title, count(log.path) as views
     from articles, log where articles.slug = substring(log.path, 10)
@@ -19,10 +20,11 @@ def most_popular_articles():
 def most_popular_authors():
     the_cursor.execute('''select authors.name, count(log.path) as views
     from authors, articles, log
-    where articles.slug = substring(log.path, 10) and articles.author = authors.id
-    group by authors.name order by views desc limit 3;''')
+    where articles.slug = substring(log.path, 10)
+    and articles.author = authors.id
+    group by authors.name order by views desc;''')
     result = the_cursor.fetchall()
-    print("Most popular three authors of all time:\n")
+    print("Most popular authors of all time:\n")
     for x in result:
         print(x[0]+" — "+str(x[1])+" views")
     print("__________________________________________________\n")
@@ -34,13 +36,15 @@ def error_days():
     the_cursor.execute('''create view errordayviews as select date(time),
     count(date(time)) from log
     where status > '400' group by date order by date desc;''')
-    the_cursor.execute('''select * from alldayviews, errordayviews
+    the_cursor.execute('''select alldayviews.date,
+    round(((errordayviews.count*100.0) / alldayviews.count), 3) as rate
+    from alldayviews, errordayviews
     where alldayviews.date = errordayviews.date
     and ((alldayviews.count*0.01) <  errordayviews.count);''')
     result = the_cursor.fetchall()
     print("days where more than 1% of requests lead to errors:\n")
-    print(result[0][0], "—", round(((result[0][3] / result[0][1]) * 100), 1),
-          "% errors")
+    for x in result:
+        print(x[0], "—", x[1],"% errors")
 
 
 most_popular_articles()
